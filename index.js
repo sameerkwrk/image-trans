76df72ff0926c026e9116cb99d8a6e68487c17f3
Sameer <jayadakhan0786@gmail.com>
2022-10-01 19:21:06 +0530

    up-to-date

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import nunjucks from "nunjucks";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import PDFDocument from "pdfkit";
+import { randomBytes } from "crypto";
+import sizeOf from "buffer-image-size";
 import cors from "cors";
 import { converter } from "./functions/converter.js";
 import multer from "multer";
@@ -14,6 +17,26 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
+
+const converter = (images, res) => {
+  images.splice(1, 1);
+  const pdf = new PDFDocument({
+    size: "a5",
+    autoFirstPage: false,
+  });
+  const tocken = randomBytes(6).toString("hex") + ".pdf";
+  pdf.info["Title"] = tocken;
+  images.forEach((element) => {
+    const dimensions = sizeOf(element.buffer);
+    pdf.addPage().image(element.buffer, {
+      align: "center",
+      valign: "center",
+      fit: [dimensions.height, dimensions.width],
+    });
+  });
+  pdf.pipe(res);
+  pdf.end();
+};
 
 nunjucks.configure(resolve(__dirname, "templates"), {
   express: app,
